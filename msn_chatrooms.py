da943419bc5d9f9aa00e4abd40cf96e80db9dff4
zmic <zmiczmic@hotmail.com>
2002-05-11 00:00:00 +0000

    pyMSNChat-1.0.0

--- msn_chatrooms.py
+++ msn_chatrooms.py
@@ -1,15 +1,41 @@
-###############################################################
-import socket, select, re, md5
+"""
+
+msn_chatrooms.py
+
+Copyright (C) 2002  zmic  (zmiczmic@hotmail.com)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+"""
 
 
-def pop_front (L):
-  if L:
-    return L.pop (0)
-  return None
+
+import socket, select, re, md5, urllib
+import passport
+
+
 
 #====================================================================================================
 
 def challenge_1 (challenge):
+  # 
+  #  Gabriel the archangel appeared to me last night, and revealed these strings
+  #
+  #  Be careful if you're not familiar with Python. An 'r' in front of a string means that 
+  #  the string should be taken literally (i.e. without interpreting \ as the escape character)
+  #
   c1 = "edp{}e|wxrdse}}u666666666666666666666666666666666666666666666666" + challenge
   a1 = md5.new(c1).digest ()
   c2 = "\x0f\x0e\x1a\x11\x17\x0f\x16\x1d\x12\x18\x0e\x19\x0f\x17\x17\x1f" + r"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" + a1
@@ -19,63 +45,127 @@ def challenge_1 (challenge):
 
 #====================================================================================================
 
-regexps = {
+numbered_commands = {
   # :TK2CHATWBA09 613 nick :207.68.167.162 6667
   613 : re.compile (r'(?P<nick>\S+) :(?P<server>\S+) (?P<port>\S+)')
-
 }
 
+other_commands = [
+  re.compile (r':(?P<nick>.*)!(?P<id>.*@GateKeeperPassport) (?P<command>PRIVMSG) (?P<to>.+) :(?P<message>.+)\r\n'),
+  re.compile (r':(?P<nick>.*)!(?P<id>.*@GateKeeperPassport) (?P<command>JOIN) ((\w,)*\w) :(?P<channel>.+)'),
+  re.compile (r':(?P<nick>.*)!(?P<id>.*@GateKeeperPassport) (?P<command>WHISPER) (?P<channel>.+) (?P<to>.+) :(?P<message>.+)\r\n'),
+  re.compile (r':(?P<nick>.*)!(?P<id>.*@GateKeeperPassport) (?P<command>PART) (?P<channel>.+)\r\n'),
+]
 
-re_command = re.compile (r'\S+ (\d+) ?(.*)')
+re_command = re.compile (r'^\S+ (\d{1,5}) ?(.*)')
 
 class Line:
   pass
 
 def parse_line (line_in):
+  if not line_in:
+    return None
   line = Line ()
   line.line = line_in
   line.command = 0
-  mo = re_command.search (line_in)
+  mo = re_command.match (line_in)
   if mo:
     line.command = int (mo.group(1))
-    if regexps.has_key (line.command):
-      mo = regexps [line.command].match (mo.group(2))
-      for x,y in mo.groupdict ().items ():
-        setattr (line, x, y)
+    if numbered_commands.has_key (line.command):
+      mo = numbered_commands [line.command].match (mo.group(2))
+      line.__dict__.update(mo.groupdict ())
+  else:
+    for r in other_commands:
+      mo = r.search (line_in)
+      if mo:
+        line.__dict__.update(mo.groupdict ())
+	break
   return line
+
+def pop_front (L):
+  if L:
+    return L.pop (0)
+  return None
+
+#====================================================================================================
+
+unpack = {
+'\\' : '\\',
+'b' : ' ',
+'t' : '\t',
+'r' : '\r',
+'n' : '\n',
+'c' : ',',
+'0' : chr(0)
+}
+
+pack = {}
+for x,y in unpack.items ():
+  pack[y] = '\\' + x
+
+# when receiving from server
+def unpack_binary_data (data):
+  #print '-> %d %r'%(len(data),data)
+  unpacked = ''
+  translate_next = 0
+  for x in data:
+    if translate_next:
+      unpacked += unpack [x]
+      translate_next = 0
+    elif x == '\\':
+      translate_next = 1
+    else:
+      unpacked += x
+  #print '<- %d %r'%(len(unpacked),unpacked)
+  return unpacked 
+
+# when sending to sever
+def pack_binary_data (data):
+  #print '-> %d %r'%(len(data),data)
+  packed = ''
+  for x in data:
+    packed += pack.get (x,x)
+  #print '<- %d %r'%(len(packed),packed)
+  return packed
+    
 
 #====================================================================================================
 
 class Msn_chatroom_connection:
-  def __init__ (self, server, port, channel = None):
+  def __init__ (self, server, port, MSPAuth, MSPProf, MSNREGCOOKIE, channel = None):
     
+    re_challenge = re.compile (r'GKSSP\\0..?\\0\02\\0\\0\\0\02\\0\\0\\0(.*)\r\n')
+
     if channel:
       #
-      # first make an connection to ask on which server the channel lives
+      # first make a connection to ask on which server the channel lives
       #
       self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       self.socket.connect ((server, port))
       self.fd = self.socket.fileno ()
       self.lines_in = []
       self.buffer = ''
-      self.socket.send ("IRCVERS IRC6 MSN-OCX!2.03.0204.0801\r\n")
+      self.socket.send ("IRCVERS IRC6 MSN-OCX!2.03.0204.3001\r\n")
       self.socket.send ("AUTH GateKeeper I :GKSSP\\0\x03r\x02\\0\\0\\0\x01\\0\\0\\0\r\n")
-      challenge = self.next_line ().rstrip()
-      challenge = challenge [-8:]
+      challenge = unpack_binary_data (re_challenge.search (self.next_line ().line).group(1))
       answer = challenge_1 (challenge)
-      answer = 'AUTH GateKeeper S :GKSSP\\0\\0\\0\x02\\0\\0\\0\x03\\0\\0\\0' + answer + '\xb2\x02"\xbd\xdd\x94sO\xae6\x03j\xfb\xe7\xaal\r\n' 
+      answer = 'AUTH GateKeeper S :GKSSP\\0\\0\\0\x02\\0\\0\\0\x03\\0\\0\\0' + pack_binary_data(answer) + 'Sm(\xe4HS\xc1M\x84\x7f\x82\x93\xf9\x8dUC\r\n' 
       self.socket.send (answer)
       self.next_line ()
       self.socket.send ('NICK nick\r\n')
       self.socket.send ('FINDS %' +'%s\r\n'%channel)
       while 1:
-        line = parse_line (self.next_line())
+        line = self.next_line()
         if line.command == 613:
           break
+	if line.command == 702:
+	  raise 'channel not found'
       server = line.server
       port = int (line.port)
     
-    print server, port
+    #
+    #  now connect to the irc server
+    #
     self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.socket.connect ((server, port))
     self.fd = self.socket.fileno ()
@@ -84,14 +174,21 @@ class Msn_chatroom_connection:
     self.socket.send ('IRCVERS IRC6 MSN-OCX!2.03.0204.0801 chat.msn.be\r\n')   
     self.next_line ()
     self.socket.send ('AUTH GateKeeperPassport I :GKSSP\\0\x03r\x02\\0\\0\\0\x01\\0\\0\\0\r\n')
-    challenge = self.next_line ().rstrip()
-    challenge = challenge [-8:]
+    challenge = unpack_binary_data (re_challenge.search (self.next_line ().line).group(1))
     answer = challenge_1 (challenge)
-    answer = 'AUTH GateKeeperPassport S :GKSSP\\0\xcc\xcc\x02\\0\\0\\0\x03\\0\\0\\0' + answer + '\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\r\n'
+    answer = 'AUTH GateKeeperPassport S :GKSSP\\0\xcc\xcc\x02\\0\\0\\0\x03\\0\\0\\0' + pack_binary_data (answer) + '\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\r\n'
     self.socket.send (answer)
     self.next_line ()
+    answer = 'AUTH GateKeeperPassport S :%08X%s%08X%s\r\n'%(len(MSPAuth),MSPAuth,len(MSPProf),MSPProf)
+    self.socket.send (answer)
+    self.next_line ()
+    self.socket.send ('PROP $ MSNREGCOOKIE :%s\r\n'%MSNREGCOOKIE)
+    self.next_line ()
+    self.socket.send ("PROP $ MSNPROFILE :0\r\n")
+    if channel:
+      self.socket.send ("JOIN %" + channel + "\r\n")
+    self.font = '\x01S \x01\x01Tahoma;0'
 
-     
   def next_line (self, timeout=None):
     if self.lines_in:
       return pop_front (self.lines_in)
@@ -111,13 +208,75 @@ class Msn_chatroom_connection:
       if pos==-1: 
         break
       line = self.buffer[0:pos+1]
-      print line[:-1]
-      self.lines_in.append (line)
+      print_line = '%r' % line[:-1]
+      print print_line[1:-1]
+      if line[:4] == 'PING':
+        # handle ping silently
+        self.socket.send ('PONG \r\n')
+      else:
+        self.lines_in.append (parse_line (line))
       self.buffer = self.buffer[pos+1:]
     return 1
+  
+  def message (self, channel, message, font = None):
+    # PRIVMSG %#Boston :\x01S \x0b\x01Palatino\\bLinotype;0 Hi Folks\x01\r\n
+    if not font:
+      font = self.font
+    s = "PRIVMSG %%%s :%s %s\x01\r\n" % (channel, font, message)
+    self.socket.send (s)
+
+  def whisper (self, channel, nick, message, font = None):
+    # WHISPER %#Flapuit __zmic__ :\x01S \x0b\x03Tahoma;0 hallo sweetie\x01\r\n
+    if not font:
+      font = self.font
+    s = "WHISPER %%%s %s :%s %s\x01\r\n" % (channel, nick, font, message)
+    self.socket.send (s)
+
+  def mode (self, channel, mode):
+    #MODE %#Flapuit +m
+    s = "MODE %%%s %s\r\n" % (channel, mode)
+    self.socket.send (s)
 
 
+
+#=====================================================================================================
+#
+#     main
+#
 if __name__ == '__main__':
-  C = Msn_chatroom_connection ('207.68.167.253', 6667, "#SocialTalk")
+  
+  
+  #
+  #  your account information
+  #
+  login    = 'cardinal_richelieu'
+  domain   = 'hotmail.com'
+  password = my_password
+
+  #
+  #  Try to retrieve PassportTicket, PassportProfile, MSNREGCOOKIE from the account information.
+  #  Should this fail, set 'try_to_find_MSNREGCOOKIE' to 0. In order to find MSNREGCOOKIE, open
+  #  a chatbox on chat.msn.com with the account that you want to use, and look in the html source for:
+  #  
+  #     "MSNREGCookie"  VALUE="just copy-paste this"  
+  #
+  #  This value should never change as long as you keep the same nickname (?). You will also find
+  #  a usable PassportTicket and PassportProfile at the same place, should all else fail.
+  #
+  PassportTicket, PassportProfile, MSNREGCOOKIE = passport.passport_login (login, domain, password, try_to_find_MSNREGCOOKIE = 1)
+
+
+  #
+  #  connect to the server that has channel '#ChatAtlanta3'
+  #
+  C = Msn_chatroom_connection ('207.68.167.253', 6667, PassportTicket, PassportProfile, MSNREGCOOKIE, "#ChatAtlanta3")
+
+  
+  #
+  #  just go into a receive loop and don't react to anything
+  #
   while 1:
     r = C.next_line (1)
+
+
+
---
+++ passport.py
@@ -0,0 +1,193 @@
+"""
+
+passport.py
+
+Copyright (C) 2002  zmic  (zmiczmic@hotmail.com)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+"""
+
+
+import urllib,urllib,os,re,sys,random,time,httplib
+import socket,select,thread,Queue,types
+
+#os.environ["http_proxy"]="http://proxy.pandora.be:8080"
+#os.environ["http_proxy"]="http://127.0.0.1:5555"
+
+
+#  search list of strings on rexep
+def search_list (L, regexp, all = 0):
+  if type (L) is types.StringType:
+    L = [L]
+  results = []
+  for x in L:
+    mo = regexp.search (x)
+    if mo:
+      G = mo.groups ()
+      if len (G) == 1:
+        G = G[0]
+      if not all:
+        return G
+      results.append (G)
+  return results 
+
+
+
+re_cookie_auth = re.compile (r'Set-Cookie: MSPAuth=(.*?);')
+re_cookie_prof = re.compile (r'Set-Cookie: MSPProf=(.*?);')
+
+re_t = re.compile (r'&t=(.*?)&')
+re_p = re.compile (r'&p=(.*?)&')
+re_url = re.compile (r'URL=(.*)"')
+re_set_cookie = re.compile (r'Set-Cookie: (.*?)\r\n')
+re_location = re.compile (r'Location: (.*)\r\n')
+re_src = re.compile (r"src='(chatroom_ui.*?)'")
+re_MSNREGCOOKIE = re.compile ('NAME="MSNREGCookie".*?VALUE="(.*?)"')
+
+
+
+def handle_302 (url, cookies, depth = ''):
+   print depth + url[0:50] + '...'
+   opener=urllib.URLopener()
+   opener.addheader("User-Agent", r"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; .NET CLR 1.0.3705)")
+   cookie = ''
+   for x,y in cookies.items ():
+     cookie += '%s=%s; ' %(x,y)
+   opener.addheader('Cookie',cookie[:-1])
+   try:
+     data = opener.open(url).read()
+     MSNREGCOOKIE = search_list (data, re_MSNREGCOOKIE)
+     if MSNREGCOOKIE:
+       # well done, thou faithful servant
+       return MSNREGCOOKIE
+     url = 'http://chat.msn.be/' + search_list (data, re_src)
+     return handle_302 (url, cookies, depth + '  ')
+   except IOError, arg:
+     for s in search_list (arg[3].headers, re_set_cookie, 1):
+       for x in s.split (';'):
+         m = x.split ('=')
+	 if len (m) == 2:
+	   cookies[m[0]]= m[1]
+     url = search_list (arg[3].headers, re_location)
+     return handle_302 (url, cookies, depth + '  ')
+
+#
+#
+#  httplib and urllib seem to be buggy on SSL-connections to IIS   :/
+#  If you manage to make this work with standard Python modules, please let me know.
+#  M2Crypto at http://www.post1.com/home/ngps/m2/
+#
+from M2Crypto import httpslib
+
+
+def do_https (host, post, body, try_to_find_MSNREGCOOKIE):
+   print "SSL connection to " + post[:40] + '...'
+   host= host + ":443"
+   hs = httpslib.HTTPS()
+   #hs.set_debuglevel(4)
+   hs.connect(host)
+   hs.putrequest('POST',post)
+   hs.putheader('Content-Length', str(len(body)))
+   hs.putheader('Accept-Encoding', 'identity')
+   hs.putheader('Accept',r'image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/vnd.ms-excel, application/msword, */*')
+   hs.putheader('Referer',r'http://login.passport.com')
+   hs.putheader('Accept-Language',r'nl-be')
+   hs.putheader('Content-Type',r'application/x-www-form-urlencoded')
+   hs.putheader('Proxy-Connection',r'Keep-Alive')
+   hs.putheader('User-Agent',r'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; .NET CLR 1.0.3705)')
+   hs.putheader('Host',r'loginnet.passport.com')
+   hs.putheader('Pragma',r'no-cache')
+   hs.endheaders()
+   hs.send(body)
+   
+   status, reason, header= hs.getreply()
+   data=hs.getfile().read()
+   
+   MSPAuth = search_list (hs.headers.headers, re_cookie_auth)
+   MSPProf = search_list (hs.headers.headers, re_cookie_prof)
+   t = search_list (data, re_t)
+   p = search_list (data, re_p)
+   url = search_list (data, re_url)
+   
+   if not MSPAuth and MSPProf:
+     raise "Could not find auth/profile cookie in HTTPS response."
+   print "Logged in succesfully!"
+
+   #
+   #  at this point, we have the auth and profile cookie.
+   #
+   if try_to_find_MSNREGCOOKIE:
+     print "trying to retrieve MSNREGCOOKIE (may timeout)"
+     MSNREGCOOKIE = handle_302 (url, {'MSPAuth':MSPAuth, 'MSPProf':MSPProf, 'hasOCX':'2%2C03%2C0204%2C0801'})
+   else:
+     MSNREGCOOKIE = None
+
+
+   return t, p, MSPAuth, MSPProf, MSNREGCOOKIE
+
+
+
+def passport_login (login, domain, password, try_to_find_MSNREGCOOKIE):
+
+  # 
+  #  This code is likely to break whenever Microsoft change the html of their chat.msn site,
+  #  but that's just the way it is.
+  #
+  
+  opener=urllib.FancyURLopener()
+  opener.addheader("User-Agent", r"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; .NET CLR 1.0.3705)")
+  lines = opener.open(r"http://chat.msn.be").readlines ()
+  re_some_chatroom = re.compile (r'<a href="(http://chat.msn.be/chatroom.msnw\?rhx=.*?;rhx1=.*?)"') 
+  chatroom_url = search_list (lines, re_some_chatroom)
+  if chatroom_url:
+    print "Opening url to some chatroom : ",chatroom_url[:40]+'...'
+    lines = opener.open(chatroom_url).read ()
+  
+    #
+    # search for the action that does the login POST
+    #
+    re_login = re.compile (r'name="hotmail_com" action="(https://(.*?)/.*?)"')
+    mo = re_login.search (lines)
+    if mo:
+      data = {
+        'login':login,
+        'domain':domain,
+        'passwd':password,
+        'sec':'0',
+        'mspp_shared':'0'
+      }
+      data = urllib.urlencode(data)
+      t, p, MSPAuth, MSPProf, MSNREGCOOKIE = do_https (mo.group(2), mo.group (1), data, try_to_find_MSNREGCOOKIE)
+      return t, p, MSNREGCOOKIE
+
+
+if __name__ == '__main__':
+  MSPAuth, MSPProf, MSNREGCOOKIE = passport_login ('Jim', 'hotmail.com', jims_password, 1)
+  print "MSPAuth = ",MSPAuth
+  print "MSPProf = ",MSPProf
+  print "MSNREGCOOKIE = ",MSNREGCOOKIE
+
+
+  
+
+
+
+
+
+
+
+
+
